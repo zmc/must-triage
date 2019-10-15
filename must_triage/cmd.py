7254bd572a8240708f51b0fb5d5404ed7b87e24a
Zack Cerza <zack@redhat.com>
2019-10-15 10:06:41 -0600

    Add -o flag (default json, yaml available)
    Sem-Ver: feature
    
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- must_triage/cmd.py
+++ must_triage/cmd.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import yaml
 
 from must_triage import formatters
 from must_triage import inspectors
@@ -11,6 +12,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=dir_or_file)
     parser.add_argument('-q', '--quiet', action='store_true')
+    parser.add_argument(
+            '-o', '--out', choices=['json', 'yaml'], default='json')
     return parser.parse_args()
 
 
@@ -31,4 +34,8 @@ def main():
         inspector = inspector_cls(**inspector_args)
         inspectors.merge_interests(interests, inspector.inspect())
 
-    print(json.dumps(interests, indent=2, default=formatters.json_serialize))
+    if args.out == 'json':
+        print(json.dumps(
+            interests, indent=2, default=formatters.json_serialize))
+    elif args.out == 'yaml':
+        print(yaml.safe_dump(interests, default_flow_style=False))
