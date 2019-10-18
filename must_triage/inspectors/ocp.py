741f07f97ccf63a1bd3304338db2a159ab269d9c
Zack Cerza <zack@redhat.com>
2019-10-18 15:29:06 -0600

    Speed up processing w/ async + process pools
    On my four-core laptop, this yields a 3.5x improvement.
    
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- must_triage/inspectors/ocp.py
+++ must_triage/inspectors/ocp.py
@@ -1,5 +1,8 @@
 import yaml
 
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
+
 import must_triage.fs as fs
 import must_triage.inspectors as inspectors
 
@@ -7,36 +10,63 @@ from must_triage.inspectors.base import Inspector
 
 
 class OCP(Inspector):
-    def inspect(self):
+    async def inspect(self):
         self.interests = dict()
         yamls = fs.find(self.root, lambda p: fs.has_ext(p, ['.yaml', '.yml']))
-        inspectors.merge_interests(self.interests, self.inspect_yamls(yamls))
+        with ProcessPoolExecutor() as executor:
+            self.executor = executor
+            interests = await self.inspect_yamls(yamls)
+        inspectors.merge_interests(
+                self.interests,
+                interests,
+        )
         return self.interests
 
-    def inspect_yamls(self, paths):
+    async def inspect_yamls(self, paths):
         if not paths:
             return dict()
         interests = dict()
-        for path in self._progress_class("Reading OCP files").iter(paths):
-            interests[path] = list()
-            with open(path) as fd:
-                try:
-                    obj = yaml.safe_load(fd)
-                except (yaml.scanner.ScannerError, yaml.parser.ParserError):
-                    interests[path].append("Failed to parse YAML content")
-                    continue
-            pods = list()
-            if obj['kind'].lower() == 'pod':
-                pods.append(obj)
-            elif obj['kind'].lower() == 'podlist':
-                pods.extend(filter(
-                    lambda o: o['kind'].lower() == 'pod',
-                    obj['items']
-                ))
-            interests[path].extend(map(self.pod_ready, pods))
+        #bar = self._progress_class("Reading OCP files", max=len(paths))
+        bar = None
+        loop = asyncio.get_running_loop()
+        futures = [
+            loop.run_in_executor(self.executor, OCP._inspect_yaml, path, bar)
+            for path in paths
+        ]
+        results = await asyncio.gather(*futures, return_exceptions=False)
+        for result in results:
+            interests.update(result)
+        if bar:
+            bar.finish()
         return interests
 
-    def pod_ready(self, obj):
+    @staticmethod
+    def _inspect_yaml(path, bar=None):
+        result = {path: list()}
+        with open(path) as fd:
+            try:
+                obj = yaml.safe_load(fd)
+            except (yaml.scanner.ScannerError, yaml.parser.ParserError):
+                result[path].append("Failed to parse YAML content")
+                if bar:
+                    bar.next()
+                return result
+        pods = list()
+        if obj['kind'].lower() == 'pod':
+            pods.append(obj)
+        elif obj['kind'].lower() == 'podlist':
+            pods.extend(filter(
+                lambda o: o['kind'].lower() == 'pod',
+                obj['items']
+            ))
+        result[path].extend(map(OCP.pod_ready, pods))
+        # print(result)
+        if bar:
+            bar.next()
+        return result
+
+    @staticmethod
+    def pod_ready(obj):
         result = list()
         status = obj['status']
         try:
--- must_triage/inspectors/ocs.py
+++ must_triage/inspectors/ocs.py
@@ -9,33 +9,40 @@ from must_triage.inspectors.base import Inspector
 
 
 class OCS(Inspector):
-    def inspect(self):
+    async def inspect(self):
         self.interests = dict()
         jsons = fs.find(
             self.root,
             lambda p: re.match('.*--format_json(-pretty)?$', p)
         )
-        inspectors.merge_interests(self.interests, self.inspect_jsons(jsons))
+        inspectors.merge_interests(
+            self.interests,
+            await self.inspect_jsons(jsons),
+        )
         return self.interests
 
-    def inspect_jsons(self, paths):
+    async def inspect_jsons(self, paths):
         if not paths:
             return dict()
         interests = dict()
         for path in self._progress_class("Reading OCS files").iter(paths):
-            interests[path] = list()
-            if os.stat(path).st_size == 0:
-                interests[path].append("File is empty")
-                continue
-            with open(path) as fd:
-                try:
-                    obj = json.load(fd)
-                except json.decoder.JSONDecodeError:
-                    interests[path].append("Failed to parse JSON content")
-                    continue
-            if 'health_detail' in path:
-                interests[path].extend(self.unhealthy(obj))
+            interests[path] = await self._inspect_json(path)
         return interests
+
+    async def _inspect_json(self, path):
+        result = list()
+        if os.stat(path).st_size == 0:
+            result.append("File is empty")
+            return result
+        with open(path) as fd:
+            try:
+                obj = json.load(fd)
+            except json.decoder.JSONDecodeError:
+                result.append("Failed to parse JSON content")
+                return result
+        if 'health_detail' in path:
+            result.extend(self.unhealthy(obj))
+        return result
 
     def unhealthy(self, obj):
         result = list()
